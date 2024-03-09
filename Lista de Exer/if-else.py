9b85664e8b3382927633884743bcdd2ac905a307
mariana.n.dourado@gmail.com <mariana.n.dourado@gmail.com>
2024-03-08 21:19:27 -0300

    exercicios

---
+++ Lista de Exer/if-else.py
@@ -0,0 +1,32 @@
+#Exer01
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite outro número: "))
+
+if n1 > n2: 
+    print(f"O seu maior número é {num1}")
+else: 
+    print  (f"Seu menor número é {num2}")
+
+
+'''---'''
+#Exer02
+ano = int(input("Digite seu ano de nascimento: "))
+if ano <= 2008: 
+    print("Você poderá votar esse ano!! Faça o L ou Arma")
+
+else: 
+    print("Espere mais um pouco, e poderá votar!")
+
+'''---'''
+#Exer03
+senha = input("Digite sua senha: ")
+if senha == 1234: 
+    print ("ACESSO PERMITIDO 💋")
+else: 
+    print("ACESSO NEGADO 😢")
+
+'''---'''
+#Exer04
+fruta = int(input("Digite quantas maças você comprou na feira hoje? "))
+if fruta >= 12:
+    print(f"O valor da sua compra foi de {feira}")
