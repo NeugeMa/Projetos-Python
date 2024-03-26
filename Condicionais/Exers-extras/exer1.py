6a93297d6a8ca79ab15adae9019f035c1d82e23a
NeugeMa <mariana.n.dourado@gmail.com>
2024-03-26 15:32:01 -0300

    mais exercícios de condicionais :)

---
+++ Condicionais/Exers-extras/exer1.py
@@ -0,0 +1,9 @@
+''' Faça um programa que peça para o usuário inserir uma idade e mostre na tela se ele é maior de idade ou não. '''
+
+idade = int(input("Digite sua idade:"))
+if idade >= 18:
+    print("Você é maior de idade")
+else: 
+    print("Sai daqui, doido")
+    
+# Link exercícios: https://medium.com/reflexão-computacional/estruturas-condicionais-if-elif-e-else-c022faace83f
