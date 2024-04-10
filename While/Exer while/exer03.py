b90b90ad80c7aec6d3a66131ad3c7ca84a436c51
NeugeMa <mariana.n.dourado@gmail.com>
2024-04-10 19:52:10 -0300

    aaaa

--- While/Exer while/exer03.py
+++ While/Exer while/exer03.py
@@ -4,6 +4,14 @@ de 3% e que a população de B seja 200000 habitantes com uma taxa de cresciment
 
 Faça um programa que calcule e escreva o número de anos necessários para que a população do país A ultrapasse ou iguale a população
 do país B, mantidas as taxas de crescimento. 
-
 '''
 
+a = 80000
+b = 200000
+ano = 0
+
+while a < b: 
+    a += a*1.03
+    b += b*1.015
+    ano += 1 
+    print(f"O país A terá {ano}") 
