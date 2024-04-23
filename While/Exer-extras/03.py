41a2aac2fc609eeb99647dadc438ec11d9f51360
NeugeMa <mariana.n.dourado@gmail.com>
2024-04-22 22:46:14 -0300

    Adicionando exercícios extras de while

---
+++ While/Exer-extras/03.py
@@ -0,0 +1,10 @@
+'''
+3. Faça um programa, utilizando while e listas, que permita o usuário escrever o nome de cinco pessoas 
+e os mostre na tela.
+'''
+
+
+qtd = 1
+while qtd < 10: 
+    nome = input(f"Diga o {qtd} nome: ")	
+    
